f1dd1120d3a76bcc3f12cd5aa5aa48628fe69dfd
Candice <candice.vernacchio@gmail.com>
2021-11-30 19:23:16 -0700

    commit tree 591948d2986763bce88745561c5f127c7d656553

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+function createEmployeeRecord(row) {
+    return {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(employeeData) {
+    return employeeData.map(function(row) {
+        return createEmployeeRecord(row)
+    })
+}
+
+function createTimeInEvent(employee, dateStamp) {
+    let [date, hour] = dateStamp.split(' ')
+    
+    employee.timeInEvents.push({
+        type: 'TimeIn',
+        hour: parseInt(hour, 10),
+        date,
+    })
+    return employee
+ }
+
+ function createTimeOutEvent(employee, dateStamp) {
+    let [date, hour] = dateStamp.split(' ')
+    
+    employee.timeOutEvents.push({
+        type: 'TimeOut',
+        hour: parseInt(hour, 10),
+        date,
+    })
+    return employee
+ }
